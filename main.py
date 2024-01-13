014aded7e8991c57b2a8920e0e5e31063ab78e0b
Mehar Khan Niazi <58723599+meharkhanniazi@users.noreply.github.com>
2024-01-13 19:19:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ def index():
     return render_template('index.html', result=False)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug = True)
