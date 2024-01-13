1c3b75094cb56ffc4c199f606e66c8acb7b632b8
Mehar Khan Niazi <58723599+meharkhanniazi@users.noreply.github.com>
2024-01-13 18:53:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template, request
+import requests, os
+
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    if request.method == 'POST':
+        from_currency = request.form.get('from_currency')
+        to_currency = request.form.get('to_currency')
+        amount = float(request.form.get('amount'))
+        response = requests.get(f'https://v6.exchangerate-api.com/v6/cb456cf692ff5e86da9404ff/latest/{from_currency}')
+        res = response.json()['conversion_rates'][to_currency]
+        convertedAmt = round(res * amount,2)
+        
+        return render_template('index.html', result=True, converted_amount=convertedAmt, to_currency=to_currency)
+
+    return render_template('index.html', result=False)
 
 
 if __name__ == '__main__':
